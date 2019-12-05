207254fa0d0e9ce949cd57d54f63adfc0f01c0e4
Jonathan <jonathanmdoyler@gmail.com>
2019-12-05 17:54:44 +0000

    Updates

---
+++ precache-manifest.8fa306187ae81ca5e964eb623a1987da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec74139c408270913e1ab096246153e",
+    "url": "/WorldHandicapCalculator/index.html"
+  },
+  {
+    "revision": "d44db2786d7acb678772",
+    "url": "/WorldHandicapCalculator/static/css/main.e54c6feb.chunk.css"
+  },
+  {
+    "revision": "abe33248fa107eaf781f",
+    "url": "/WorldHandicapCalculator/static/js/2.5a2a133c.chunk.js"
+  },
+  {
+    "revision": "d44db2786d7acb678772",
+    "url": "/WorldHandicapCalculator/static/js/main.8edbe695.chunk.js"
+  },
+  {
+    "revision": "4fdd95da5fcfb6e4ab1a",
+    "url": "/WorldHandicapCalculator/static/js/runtime-main.c28d8d07.js"
+  }
+]);
